4a7f7be1c99227e5693277d54bb871d3ee186d64
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-13 20:10:42 -0600

    wip

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,3 @@
 <?php
 
-function article_path($article = '')
-{
-    return trim(resource_path("articles/{$article}"));
-}
+//
