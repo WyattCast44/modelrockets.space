fbc33aaca828c77ffbc0dbb72e628611a23c24e3
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-19 22:45:30 -0600

    wip

--- database/seeds/BoardsTableSeeder.php
+++ database/seeds/BoardsTableSeeder.php
@@ -29,6 +29,27 @@ class BoardsTableSeeder extends Seeder
             'public' => true,
             'allow_new_public_threads' => false,
         ]);
+
+        factory(Board::class)->create([
+            'name' => 'Mid Power Rocketry',
+            'description' => 'Anything and everything, mid power rocketry!',
+            'public' => true,
+            'allow_new_public_threads' => true,
+        ]);
+
+        factory(Board::class)->create([
+            'name' => 'High Power Rocketry',
+            'description' => 'Anything and everything, high power rocketry!',
+            'public' => true,
+            'allow_new_public_threads' => true,
+        ]);
+
+        factory(Board::class)->create([
+            'name' => 'ITAR/USML',
+            'description' => 'Anything and everything related to ITAR/USML.',
+            'public' => true,
+            'allow_new_public_threads' => true,
+        ]);
     }
 
     public function createRandomBoards($number = 5)
