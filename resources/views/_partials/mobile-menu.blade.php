25b4f132ad75072f0a5c8bb877f3850baa7490eb
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 16:22:27 -0600

    wip

--- resources/views/_partials/mobile-menu.blade.php
+++ resources/views/_partials/mobile-menu.blade.php
@@ -10,15 +10,15 @@
         <div class="flex mt-3 text-center justify-center items-center">
             @auth
                 <a href="{{ route('users.show', auth()->user()) }}" class="block p-3 text-xl text-white hover:text-white hover:underline">My Profile</a>        
-                <a href="#" onclick="document.getElementById('logout-form').submit();" class="block p-3 text-xl text-white hover:text-white hover:underline">
+                <a href="#" onclick="document.getElementById('logout-form').submit();" class="block p-3 text-xl text-white hover:text-white hover:underline" data-turbolinks="false">
                     Logout
                 </a>
             @endauth
 
             @guest
 
-                <a href="#login" class="block p-3 text-xl text-white hover:text-white hover:underline">Login</a>        
-                <a href="#register" class="block p-3 text-xl text-white hover:text-white hover:underline">Register</a>        
+                <a href="#login" class="block p-3 text-xl text-white hover:text-white hover:underline" data-turbolinks="false">Login</a>        
+                <a href="#register" class="block p-3 text-xl text-white hover:text-white hover:underline" data-turbolinks="false">Register</a>        
 
             @endguest
         </div>
