46413f316323990cb9451ab90125779066a37c08
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 19:01:21 -0600

    wip

--- resources/views/forum/layout.blade.php
+++ resources/views/forum/layout.blade.php
@@ -8,7 +8,7 @@
             
             <div>
 
-                <label for="" class="block uppercase mb-1 text-xs text-gray-500 text-center">Filters</label>
+                <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Filters</label>
                 <div class="relative">
                     <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                         <option>All</option>
@@ -24,7 +24,7 @@
             </div>
 
             <div>
-                <label for="" class="block uppercase mb-1 text-xs text-gray-500 text-center">Boards</label>
+                <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Boards</label>
                 <div class="relative">
                         <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                             <option>General</option>
