83dd7d5dd3805f761bf7b9c05d995326496dfe83
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 18:39:57 -0600

    wip

---
+++ resources/views/layouts/_partials/head-assets.blade.php
@@ -0,0 +1,4 @@
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="{{ mix('/css/bs.css') }}" rel="stylesheet">
+<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+<script src="{{ mix('/js/app.js') }}" defer></script>
--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('page-title') - Model Rockets Space 🚀🌌</title>
-    <meta name="description" content="Model Rockets Space, for the love of rocketry.">
-    <meta property="og:site_name" content="modelrockets.space">
-    <meta property="og:locale" content="en_US"> 
-    <meta property="og:description" content="Model Rockets Space, for the love of rocketry.">
-    <meta property="og:url" content="https://modelrockets.space">
-    {{-- <meta property="og:image" content="https://modelrockets.space/images/avatar-boxed.jpg"> --}}
-    <meta name="mobile-web-app-capable" content="yes">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="{{ mix('/js/app.js') }}" defer></script>
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="{{ mix('/css/bs.css') }}" rel="stylesheet">
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    @include('layouts._partials.head-meta')
+    @include('layouts._partials.head-assets')
 </head>
 
 <body class="bg-gray-100">
-
     
     <div id="app">
 
