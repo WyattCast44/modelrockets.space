39e0057727a77054df72bc3ac26a1087eb8d8705
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 19:00:01 -0600

    wip

--- resources/views/forum/layout.blade.php
+++ resources/views/forum/layout.blade.php
@@ -25,11 +25,17 @@
 
             <div>
                 <label for="" class="block uppercase mb-1 text-xs text-gray-500 text-center">Boards</label>
-                <select name="filter" id="filter" class="form-control">
-                    <option value="all">Mid Power Rockets</option>
-                    <option value="all">High Power Rockets</option>
-                    <option value="all">Watering Hole</option>
-                </select>
+                <div class="relative">
+                        <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                            <option>General</option>
+                            <option>Watering Hole</option>
+                            <option>Low Power Rocketry</option>
+                            <option>High Power Rocketry</option>
+                        </select>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                        </div>
+                    </div>
             </div>
 
         </div> 
