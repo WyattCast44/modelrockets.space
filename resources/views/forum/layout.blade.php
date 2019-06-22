359b2ede041dd64c6d7ff5d659d112b8d13d0226
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 18:53:30 -0600

    wip

--- resources/views/forum/layout.blade.php
+++ resources/views/forum/layout.blade.php
@@ -2,11 +2,39 @@
 
 @section('content')
 
-    <header class="border-b border-solid border-gray-300 bg-gray-200 py-6 sticky top-0 shadow-md mb-8" style="z-index:1000;">
+    <header class="border-b border-solid border-gray-300 bg-gray-200 py-4 md:py-6 sticky top-0 shadow-md mb-8" style="z-index:1000;">
         
-        <div class="mx-auto" style="width:600px;max-width:90%">
+        {{-- <div class="mx-auto" style="width:600px;max-width:90%">
             <input type="text" placeholder="Search the forums..." class="form-control">    
-        </div>    
+        </div>     --}}
+
+        <div class="mx-auto flex justify-between items-center md:hidden" style="width:600px;max-width:90%">
+            
+            <div>
+
+                <div class="relative">
+                    <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                        <option>All</option>
+                        <option>Popular</option>
+                        <option>Newest</option>
+                        <option>My Threads</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                    </div>
+                </div>
+
+            </div>
+
+            <div>
+                <select name="filter" id="filter" class="form-control">
+                    <option value="all">Mid Power Rockets</option>
+                    <option value="all">High Power Rockets</option>
+                    <option value="all">Watering Hole</option>
+                </select>
+            </div>
+
+        </div> 
 
     </header>
 
