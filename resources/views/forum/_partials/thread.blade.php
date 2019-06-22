023a2fdff50bae0951e0967bae6c6cc9e7491190
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 21:44:02 -0600

    wip

--- resources/views/forum/_partials/thread.blade.php
+++ resources/views/forum/_partials/thread.blade.php
@@ -1,20 +1,24 @@
-<li class="block mb-3 border border-solid border-gray-300 bg-white py-5 px-6 rounded-lg shadow-md hover:border-2 hover:border-solid hover:border-blue-600 hover:shadow-lg">
+<li class="block mb-5 border border-solid border-gray-300 bg-white py-6 px-6 rounded-lg shadow-md hover:border-2 hover:border-solid hover:border-blue-600 hover:shadow-lg">
     
-    <div class="flex flex-wrap items-center md:justify-between">
+    <div class="flex flex-wrap md:justify-between">
 
         <!-- Thread -->
-        <div class="w-100">
+        <div class="w-100 md:w-3/4 flex flex-col justify-between">
             <a href="{{ $thread->path() }}" class="text-lg">
                 {{ $thread->title }}
             </a>
-        
+
             <p class="my-3 text-sm text-gray-600">
                 {{ Str::limit($thread->body, 225) }}
+            </p>
+
+            <p class="text-xs text-gray-600">
+                Posted {{ $thread->created_at->diffForHumans() }} by <a href="{{ route('users.show', $thread->user) }}">{{ '@' . $thread->user->username }}</a>
             </p>
         </div>
 
         <!-- Board -->
-        <div class="flex justify-center items-center md:ml-4">
+        <div class="flex justify-end items-start md:pl-4 md:w-1/4">
             <a href="{{ $thread->board->path($thread->board) }}" class="btn btn-sm px-3 btn-outline-primary rounded md:rounded-full">
                 {{ $thread->board->name }}
             </a>
--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -9,22 +9,30 @@
         <div class="flex">
 
             <!-- Filters Sidebar -->
-            {{-- <div class="w-1/6 px-3 hidden lg:block">
+            <div class="w-1/6 px-3 hidden lg:block">
                 <div>
-                    <h2 class="font-semibold text-2xl mb-3 uppercase text-gray-600">Filters</h2>
-                    <ul class="list-disc">
-                        <li>All</li>
-                        <li>New</li>
+                    <h2 class="font-semibold text-2xl mb-3 uppercase text-gray-600 text-center">Filters</h2>
+                    <ul class="text-center">
+                        <li>
+                            <a href="{{ route('forum.index') }}"
+                            class="btn btn-sm btn-outline-primary rounded-full uppercase hover:shadow-lg mb-2 shadow-sm block">
+                                All
+                            </a>
+                            <a href="?q=mine"
+                            class="btn btn-sm btn-outline-primary rounded-full uppercase hover:shadow-lg mb-2 shadow-sm block">
+                                My Threads
+                            </a>
+                        </li>     
                     </ul>
                 </div>
-            </div> --}}
+            </div>
 
             <!-- Threads Index -->
             <div class="flex-1 px-3 mx-3">
                 
                 <div>
 
-                    <h2 class="font-semibold text-xl md:text-2xl mb-3 uppercase text-gray-600">Recent Threads</h2>
+                    {{-- <h2 class="font-semibold text-xl md:text-2xl mb-3 uppercase text-gray-600">Recent Threads</h2> --}}
 
                     <ul>
 
