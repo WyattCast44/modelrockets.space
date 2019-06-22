9e10536bbf3e570ee80cf7896e135faa78194528
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 18:55:37 -0600

    wip

--- resources/views/forum/layout.blade.php
+++ resources/views/forum/layout.blade.php
@@ -12,6 +12,7 @@
             
             <div>
 
+                <label for="" class="uppercase mb-1 text-xs text-gray-500">Filters</label>
                 <div class="relative">
                     <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                         <option>All</option>
@@ -27,6 +28,7 @@
             </div>
 
             <div>
+                <label for="" class="uppercase mb-1 text-xs text-gray-500">Boards</label>
                 <select name="filter" id="filter" class="form-control">
                     <option value="all">Mid Power Rockets</option>
                     <option value="all">High Power Rockets</option>
