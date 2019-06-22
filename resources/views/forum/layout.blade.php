8f8ba7f6e81cdc010e9238bf8844b48ed500d93b
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-21 19:03:17 -0600

    wip

--- resources/views/forum/layout.blade.php
+++ resources/views/forum/layout.blade.php
@@ -11,6 +11,7 @@
                 <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Filters</label>
                 <div class="relative">
                     <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                        <option disabled selected>Select</option>
                         <option>All</option>
                         <option>Popular</option>
                         <option>Newest</option>
@@ -27,6 +28,7 @@
                 <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Boards</label>
                 <div class="relative">
                         <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                            <option disabled selected>Select</option>
                             <option>General</option>
                             <option>Watering Hole</option>
                             <option>Low Power Rocketry</option>
