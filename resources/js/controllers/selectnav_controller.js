f770ce6ac48e66db8c3034db2039cc7f6fcebc51
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-22 18:28:11 -0600

    wip

--- resources/js/controllers/selectnav_controller.js
+++ resources/js/controllers/selectnav_controller.js
@@ -1,17 +1,14 @@
 import { Controller } from "stimulus";
-import { throws } from "assert";
 
 export default class extends Controller {
     static targets = ["select"];
 
     handle(event) {
-        let queryParam = this.selectTarget.value;
-
-        this.navigate("?q=" + queryParam);
+        let destination = this.selectTarget.value;
+        this.navigate(destination);
     }
 
-    navigate(url) {
-        console.log(`hitting: {url}`);
-        window.location.href = url;
+    navigate(destination) {
+        window.location.href = destination;
     }
 }
--- resources/views/forum/layout.blade.php
+++ resources/views/forum/layout.blade.php
@@ -11,57 +11,23 @@
     </div>
 </header>
 
-<nav class="border-b border-solid border-gray-300 bg-gray-200 py-4 md:py-6 sticky top-0 shadow-lg mb-4 sm:mb-6 md:mb-12" style="z-index:1000;">
+<section class="border-b border-solid border-gray-300 bg-gray-200 py-4 md:py-6 sticky top-0 shadow-lg mb-4 sm:mb-6 md:mb-12" style="z-index:1000;">
 
-    <div class="container mx-auto flex justify-between items-center md:shidden" style="max-width:90%">
+    <div class="container md:shidden" style="max-width:90%">
         
-        <!-- Filters -->
-        <div>
+        <div class="flex justify-between items-center">
+            
+            @include('forum._partials.filter-select')
 
-            <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Filters</label>
-            <div class="relative">
-                <select data-controller="selectnav" data-target="selectnav.select" data-action="change->selectnav#handle" class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                    <option disabled selected>Select</option>
-                    <option value="all">All</option>
-                    <option value="popular">Popular</option>
-                    <option value="latest">Newest</option>
-                    <option value="mine">My Threads</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                </div>
-            </div>
+            @include('forum._partials.boards-select')
+            
+            @include('forum._partials.search')
 
-        </div>
-
-        <!-- Search -->
-        <div class="flex-1 mx-3 hidden md:block">
-
-                <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Search</label>
-                <input type="text" class="my-0 w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" placeholder="Search the forums...">
-
-        </div>
-
-        <!-- Boards -->
-        <div class="ml-2 md:ml-0">
-            <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center">Boards</label>
-            <div class="relative">
-                    <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                        <option disabled selected>Select</option>
-                        <option>General</option>
-                        <option>Watering Hole</option>
-                        <option>Low Power Rocketry</option>
-                        <option>High Power Rocketry</option>
-                    </select>
-                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                    </div>
-                </div>
         </div>
 
     </div> 
 
-</nav>
+</section>
 
     @yield('forum-page')
     
