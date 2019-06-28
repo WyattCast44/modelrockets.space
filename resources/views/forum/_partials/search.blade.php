33a6915d476cf933fd06d3b36d8a8d712acc9f2b
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-06-27 21:31:16 -0600

    wip

--- resources/views/forum/_partials/search.blade.php
+++ resources/views/forum/_partials/search.blade.php
@@ -1,6 +1,6 @@
 <div class="flex-1 hidden md:block">
 
     <label for="" class="leading-none block uppercase mb-1 text-xs text-gray-500 text-center md:text-left">Search</label>
-    <input type="text" class="my-0 w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" placeholder="Search the forums...">
+    <input type="text" class="my-0 w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" placeholder="Search the forums..." autofocus>
 
 </div>
