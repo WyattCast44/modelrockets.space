963670d369cde3014c21f38c4c1974bc896773e9
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-07-31 14:19:44 -0600

    wip

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -13,10 +13,30 @@
 
 <div class="container mx-auto pt-8 pb-16">
 
+    <!-- Actions -->
+    <div class="flex items-center mb-4 sm:mb-6 md:mb-8 justify-center md:justify-start print:hidden">
+
+        <!-- Print -->
+        <div data-controller="print">
+            <a href="#" class="btn btn-outline-primary mr-2 btn-sm" data-action="print#handle" data-turbolinks="false">🖨️ Print</a>
+        </div>
+        
+        <!-- Share -->
+        <a href="#share" class="btn btn-outline-primary btn-sm" data-turbolinks="false">📤 Share</a>
+
+    </div>
+
+    <!-- Meta -->
+    <p class="text-sm text-gray-500 my-1 md:mb-8 text-center md:text-left">
+        Last updated July 31st 2019
+    </p>
+
     <div class="markdown-body">
         {!! markdown('pages/_content/privacy') !!}
     </div>
 
 </div>
+
+@include('_partials.share')
 
 @endsection
