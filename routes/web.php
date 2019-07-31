ada7f87d28e5a3b14ada653a6de1fafd4d7a80cc
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-07-31 14:09:09 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,4 @@
 <?php
-use Illuminate\Http\Request;
-use App\Article;
 
 Auth::routes();
 
--- tests/Feature/FeaturesTest.php
+++ tests/Feature/FeaturesTest.php
@@ -4,13 +4,60 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Feature;
 
 class FeaturesTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_a_user_must_be_logged_in_to_view_create_threads_form()
+    public function test_authenticated_users_can_upvote_features()
     {
-        //
+        // Given we have a feature
+        $feature = create(Feature::class);
+
+        // And we are signed in
+        $this->signIn();
+
+        // And we visit the features page
+        $res = $this->get(route('features.index'));
+
+        // It should be okay
+        $res->assertOk();
+
+        // We should see an "upvote" btn
+        $res->assertSee('Upvote');
+
+        // And when we "upvote" the feature
+        $res = $this->post(route('features.upvote', $feature));
+
+        // We should be redirected back to the features index
+        $res->assertRedirect(route('features.index'));
+
+        // And the feature should have one upvote
+        $this->assertEquals(1, $feature->refresh()->votes->count());
+    }
+
+    public function test_unauthenticated_users_cannot_upvote_features()
+    {
+        // Given we have a feature
+        $feature = create(Feature::class);
+
+        // And we visit the features page
+        $res = $this->get(route('features.index'));
+
+        // It should be okay
+        $res->assertOk();
+
+        // We should see a login to vote btn
+        $res->assertSee('Login to vote');
+
+        // If we try to "upvote" the feature
+        $res = $this->post(route('features.upvote', $feature));
+
+        // We should be redirected to the login page
+        $res->assertRedirect('login');
+
+        // And the feature should have no upvotes
+        $this->assertEquals(0, $feature->refresh()->votes->count());
     }
 }
