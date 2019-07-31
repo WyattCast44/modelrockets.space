615723a3411d4bcb6155f2beada8e5d051320d70
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-07-31 15:20:36 -0600

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,10 @@
 
 @section('content')
 
-<header class="flex flex-col bg-gray-200 border-b border-solid border-gray-300 pt-12">
-    <div class="container flex">
-        
+<header class="flex flex-col bg-gray-200 border-b border-solid border-gray-300">
+    <div class="container py-12">
+        <h2 class="text-3xl font-light mb-4">Welcome to Model Rockets Space</h2>
+        <p class="text-xl font-light"><span class="text-red-700">⚠️ Warning, site is under construction, launch date tbd!</span></p>
     </div>
 </header>
 
