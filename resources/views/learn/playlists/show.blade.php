71bb2edce627f703e2e652d492d8d48498ee851e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-09-08 20:01:07 -0600

    wip

--- resources/views/learn/playlists/show.blade.php
+++ resources/views/learn/playlists/show.blade.php
@@ -14,13 +14,13 @@
     
         <div class="w-1/5 bg-gray-900 p-4 flex-col">
 
-            <h2 class="uppercase text-white font-semibold">{{ $playlist->name }}</h2>
+            <h2 class="uppercase text-white font-semibold mb-3">{{ $playlist->name }}</h2>
 
             <!-- Prev/Next -->
-            <div class="flex items-center justify-center my-4">
+            {{-- <div class="flex items-center justify-center my-4">
                 <a href="#" class="block w-full text-white hover:bg-gray-700 hover:text-white shadow-inner hover:no-underline px-3 py-2 mr-1 text-center bg-gray-800 rounded-lg uppercase text-sm">Prev</a>
                 <a href="#" class="block w-full text-white hover:bg-gray-700 hover:text-white shadow-inner hover:no-underline px-3 py-2 ml-1 text-center bg-gray-800 rounded-lg uppercase text-sm">Next</a>
-            </div>
+            </div> --}}
 
             <!-- Playlist videos -->
             <ul class="flex-1 list-unstyled border-solid border border-gray-800 max-w-full shadow-inner rounded-lg overflow-y-auto" style="max-height: 75%">
