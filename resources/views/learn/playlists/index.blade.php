a233d3342656020717e55cd71da3b8a7b8f38fc3
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-05-16 16:50:23 -0700

    wip

--- resources/views/learn/playlists/index.blade.php
+++ resources/views/learn/playlists/index.blade.php
@@ -5,14 +5,18 @@
 @section('content')
 
 <header class="flex flex-col py-12 mb-8 bg-gray-200 border-b border-gray-300 border-solid">
-    <div class="container flex">
+    <div class="container flex flex-col-reverse flex-wrap items-center justify-center sm:flex-col sm:flex-no-wrap">
+        
         <div class="w-3/4 mr-8">
+
             <h1 class="mb-2 text-4xl font-semibold">
                 {{ $playlist->name }}
             </h1>
+
             <p class="text-lg text-gray-700">
                 {{ $playlist->description }}
             </p>    
+            
             <div class="flex mt-5">
 
                 @if (auth()->check() && !auth()->user()->isSubscribedTo($playlist))
@@ -34,10 +38,13 @@
 
                 
             </div>
+            
         </div>
-        <div class="flex items-center justify-end object-cover">
+
+        <div class="flex items-center justify-end object-cover mb-5 sm:mb-0">
             <img src="{{ $playlist->imageUrl }}" alt="playlist cover image" class="h-48 rounded-lg shadow-lg">
         </div>
+        
     </div>
 </header>
 
--- resources/views/learn/playlists/show.blade.php
+++ resources/views/learn/playlists/show.blade.php
@@ -6,27 +6,27 @@
 
 <div class="h-screen">
 
-    <section class="bg-gray-800 flex flex-col md:flex-row justify-center items-top z-0 overflow-hidden" style="max-height: 650px;">
+    <section class="z-0 flex flex-col justify-center overflow-hidden bg-gray-800 md:flex-row items-top" style="max-height: 650px;">
 
-        <div class="flex-1 flex justify-center items-center vid-container p-4">
+        <div class="flex items-center justify-center flex-1 p-4 vid-container">
             {!! $mainVideo->embed_code !!}
         </div>
     
-        <div class="w-1/5 bg-gray-900 p-4 flex-col">
+        <div class="flex-col w-1/5 p-4 bg-gray-900">
 
-            <h2 class="uppercase text-white font-semibold mb-3">{{ $playlist->name }}</h2>
+            <h2 class="mb-3 font-semibold text-white uppercase">{{ $playlist->name }}</h2>
 
             <!-- Prev/Next -->
-            {{-- <div class="flex items-center justify-center my-4">
-                <a href="#" class="block w-full text-white hover:bg-gray-700 hover:text-white shadow-inner hover:no-underline px-3 py-2 mr-1 text-center bg-gray-800 rounded-lg uppercase text-sm">Prev</a>
-                <a href="#" class="block w-full text-white hover:bg-gray-700 hover:text-white shadow-inner hover:no-underline px-3 py-2 ml-1 text-center bg-gray-800 rounded-lg uppercase text-sm">Next</a>
-            </div> --}}
+            <div class="flex items-center justify-center my-4">
+                <a href="#" class="block w-full px-3 py-2 mr-1 text-sm text-center text-white uppercase bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 hover:text-white hover:no-underline">Prev</a>
+                <a href="#" class="block w-full px-3 py-2 ml-1 text-sm text-center text-white uppercase bg-gray-800 rounded-lg shadow-inner hover:bg-gray-700 hover:text-white hover:no-underline">Next</a>
+            </div>
 
             <!-- Playlist videos -->
-            <ul class="flex-1 list-unstyled border-solid border border-gray-800 max-w-full shadow-inner rounded-lg overflow-y-auto" style="max-height: 75%">
+            <ul class="flex-1 max-w-full overflow-y-auto border border-gray-800 border-solid rounded-lg shadow-inner list-unstyled" style="max-height: 75%">
                 @foreach ($playlist->videos as $video)
-                    <li class="block hover:bg-gray-800 rounded-lg">
-                        <a href="{{ route('learn.playlists.videos.show', ['playlist' => $playlist, 'video' => $video]) }}" class="p-4 block hover:no-underline hover:text-white text-white">
+                    <li class="block rounded-lg hover:bg-gray-800">
+                        <a href="{{ route('learn.playlists.videos.show', ['playlist' => $playlist, 'video' => $video]) }}" class="block p-4 text-white hover:no-underline hover:text-white">
                             {{ $loop->iteration }}. {{ $video->name }}
                         </a>
                     </li>
@@ -36,7 +36,7 @@
     
     </section>
 
-    <div class="bg-gray-200 shadow-inner z-10">
+    <div class="z-10 bg-gray-200 shadow-inner">
         <div class="container py-6">
             name
         </div>
